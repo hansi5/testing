8370c79e7dd876475280806d40994c5b2233cf77
hansi5 <hansi.aa.05@gmail.com>
2026-01-05 19:51:38 +0530

    commit

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 import smtplib
 import os
+from dotenv import load_dotenv
 from datetime import datetime
 
 now = datetime.now()
 
+# variables for the smtp email
+load_dotenv()
 EMAIL = os.environ.get("EMAIL")
 PASSWORD = os.environ.get("PASSWORD")
 
